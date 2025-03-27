838e34457c6fd7da7dc8a0be89190083855da1ff
Damir <dhadiulin@gmail.com>
2025-03-27 15:10:59 +0500

    Done

--- main.py
+++ main.py
@@ -2,22 +2,11 @@ import telebot
 import config
 import random
 
-math_eq = []
-x = 0
-def generate_equation():
-    for a in range(1, 1000): 
-        for b in range(1, 10000):  
-            if b % a == 0:
-                x = b // a
-                math_eq.append(f'[{a}x = {b} | Ответ x = {x}')  
-    return math_eq
-math_eq = generate_equation()
-equation = math_eq[random.randint(1, len(math_eq))][0]
-print(equation)
+
 jokes = ["Как называют человека, который продал свою печень? Обеспеченный"
-,"Почему шутить можно над всеми, кроме безногих? Шутки про них обычно не заходят", "Почему безногий боится гопников?"
-"Не может постоять за себя." , "Почему толстых женщин не берут в стриптиз?"
-"Они перегибают палку.","Почему в Африке так много болезней? Потому что таблетки нужно запивать водой.","Что сказал слепой, войдя в бар?"
+,"Почему шутить можно над всеми, кроме безногих?\nШутки про них обычно не заходят", "Почему безногий боится гопников?\n"
+"Не может постоять за себя." , "Почему толстых женщин не берут в стриптиз?\n"
+"Они перегибают палку.","Почему в Африке так много болезней?\n Потому что таблетки нужно запивать водой.","Что сказал слепой, войдя в бар?\n"
 "Всем привет, кого не видел"]
 bot = telebot.TeleBot(config.TG_API_TOKEN)
 
@@ -46,15 +35,31 @@ def send_joke(message):
 
 @bot.message_handler(commands=['math'])
 def math(message):
-    bot.send_message(message.chat.id, f" {math_eq[random.randint(1, len(math_eq))[0]]} || {math_eq[random.randint(1, len(math_eq))[1]]} ||", parse_mode='MarkdownV2')
+    a = random.randint(1, 1000)  
+    x = random.randint(1, 1000)  
+    operation = random.choice(['+', '-', '*'])
+    c = random.randint(1, 1000)  
+    if operation == '+':
+        b = a * x + c
+        equation = f"{a}x + {c} = {b}"
+        solution = (b - c) // a  
+    elif operation == '-':
+        b = a * x - c
+        equation = f"{a}x - {c} = {b}"
+        solution = (b + c) // a 
+    elif operation == '*':
+        b = a * x * c
+        equation = f"{a}x * {c} = {b}"
+        solution = b // (a * c)  # Решение для x
+    bot.send_message(message.chat.id, f"{equation} <tg-spoiler>  Ответ x = {solution} </tg-spoiler>", parse_mode='HTML')
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	if message.text.lower == "ЁУ чё как?".lower:
+	if message.text.lower() == "Ё чё как?".lower():
 		bot.send_message(message.chat.id, "Норм я")
-	if message.text.lower == "Что нового?".lower:
+	if message.text.lower() == "Что нового?".lower():
 		bot.send_message(message.chat.id, "Да ничего, всё по старому")
-	if message.text.lower == "Что делаешь?".lower:
+	if message.text.lower() == "Что делаешь?".lower():
 		bot.send_message(message.chat.id, "Да фигнёй маюсь")
 
 bot.infinity_polling()
